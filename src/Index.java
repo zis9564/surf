8f1e62e6b6eb6909b059aea45d4a3c6947f1793b
zis9564 <zis9564@gmail.com>
2023-01-22 17:19:51 +0300

    Index class reviewed and refactored.

--- src/Index.java
+++ src/Index.java
@@ -1,100 +1,151 @@
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.*;
 import java.util.concurrent.*;
-import java.util.stream.Stream;
+import java.util.stream.Collectors;
 
 public class Index {
-    TreeMap<String, List<Pointer>> invertedIndex;
 
-    ExecutorService pool;
+    private final ExecutorService pool;
+    private final Map<String, List<Pointer>> invertedIndex;
+
+    public Index() {
+        this.pool = Executors.newFixedThreadPool(3);
+        this.invertedIndex = new ConcurrentHashMap<>();
+    }
 
     public Index(ExecutorService pool) {
         this.pool = pool;
-        invertedIndex = new TreeMap<>();
+        this.invertedIndex = new ConcurrentHashMap<>();
     }
 
-    public void indexAllTxtInPath(String pathToDir) throws IOException {
-        Path of = Path.of(pathToDir);
-
-        BlockingQueue<Path> files = new ArrayBlockingQueue<>(2);
-
-        try (Stream<Path> stream = Files.list(of)) {
-            stream.forEach(files::add);
+    public void indexAllTxtInPath(String path) {
+        Objects.requireNonNull(path, "path is required.");
+        var tasks = findTextFilesInDir(path)
+                .stream()
+                .map(p -> new IndexTask(p, invertedIndex))
+                .collect(Collectors.toList());
+        try {
+            pool.invokeAll(tasks);
+        } catch (InterruptedException e) {
+            System.err.println("indexAllTxtInPath(): Error has occurred. message: " + e.getMessage());
         }
-
-        pool.submit(new IndexTask(files));
-        pool.submit(new IndexTask(files));
-        pool.submit(new IndexTask(files));
+        shutdownAndAwaitTermination(pool);
     }
 
-    public TreeMap<String, List<Pointer>> getInvertedIndex() {
+    public Map<String, List<Pointer>> getInvertedIndex() {
         return invertedIndex;
     }
 
     public List<Pointer> GetRelevantDocuments(String term) {
+        Objects.requireNonNull(term, "term is required");
         return invertedIndex.get(term);
     }
 
     public Optional<Pointer> getMostRelevantDocument(String term) {
-        return invertedIndex.get(term).stream().max(Comparator.comparing(o -> o.count));
+        Objects.requireNonNull(term, "term is required");
+        return invertedIndex.get(term)
+                .stream()
+                .max(Comparator.comparing(Pointer::getCounter));
     }
 
-    static class Pointer {
-        private Integer count;
-        private String filePath;
-
-        public Pointer(Integer count, String filePath) {
-            this.count = count;
-            this.filePath = filePath;
+    private List<String> findTextFilesInDir(String path) {
+        Objects.requireNonNull(path, "path is required.");
+        try (var stream = Files.list(Paths.get(path).toAbsolutePath())) {
+            return stream
+                    .filter(file -> !Files.isDirectory(file))
+                    .map(Path::toString)
+                    .filter(name -> name.endsWith(".txt"))
+                    .collect(Collectors.toList());
+        } catch (IOException e) {
+            System.err.println("indexAllTxtInDir(): problem occurred with path: " + path);
         }
-
-        @Override
-        public String toString() {
-            return "{" + "count=" + count + ", filePath='" + filePath + '\'' + '}';
-        }
+        return List.of();
     }
 
-    class IndexTask implements Runnable {
-
-        private final BlockingQueue<Path> queue;
-
-        public IndexTask(BlockingQueue<Path> queue) {
-            this.queue = queue;
-        }
-
-        @Override
-        public void run() {
-            try {
-                Path take = queue.take();
-                List<String> strings = Files.readAllLines(take);
-
-                strings.stream().flatMap(str -> Stream.of(str.split(" "))).forEach(word -> invertedIndex.compute(word, (k, v) -> {
-                    if (v == null) return List.of(new Pointer(1, take.toString()));
-                    else {
-                        ArrayList<Pointer> pointers = new ArrayList<>();
-
-                        if (v.stream().noneMatch(pointer -> pointer.filePath.equals(take.toString()))) {
-                            pointers.add(new Pointer(1, take.toString()));
-                        }
-
-                        v.forEach(pointer -> {
-                            if (pointer.filePath.equals(take.toString())) {
-                                pointer.count = pointer.count + 1;
-                            }
-                        });
-
-                        pointers.addAll(v);
-
-                        return pointers;
-                    }
-
-                }));
-
-            } catch (InterruptedException | IOException e) {
-                throw new RuntimeException();
+    private void shutdownAndAwaitTermination(ExecutorService pool) {
+        pool.shutdown(); // Disable new tasks from being submitted
+        try {
+            // Wait a while for existing tasks to terminate
+            if (!pool.awaitTermination(60, TimeUnit.SECONDS)) {
+                pool.shutdownNow(); // Cancel currently executing tasks
+                // Wait a while for tasks to respond to being cancelled
+                if (!pool.awaitTermination(60, TimeUnit.SECONDS))
+                    System.err.println("shutdownAndAwaitTermination(): Pool did not terminate");
             }
+        } catch (InterruptedException ie) {
+            // (Re-)Cancel if current thread also interrupted
+            pool.shutdownNow();
+            // Preserve interrupt status
+            Thread.currentThread().interrupt();
         }
+    }
+
+    private static class IndexTask implements Callable<Map<String, List<Pointer>>> {
+        private final String path;
+        private final Map<String, List<Pointer>> invertedIndex;
+
+        public IndexTask(String path,
+                         Map<String, List<Pointer>> map) {
+            this.path = Objects.requireNonNull(path, "path is required");
+            this.invertedIndex = Objects.requireNonNull(map, "map is required");
+        }
+
+        @Override
+        public Map<String, List<Pointer>> call() {
+            try (Scanner scanner = new Scanner(new File(path))) {
+                while (scanner.hasNext()) {
+                    var word = scanner.next();
+                    invertedIndex.compute(word, (key, val) -> {
+                        if (val == null) {
+                            return List.of(new Pointer(path));
+                        } else {
+                            List<Pointer> pointers = new ArrayList<>();
+                            val.stream()
+                                    .filter(p -> p.getFilePath().equals(path))
+                                    .findFirst()
+                                    .ifPresentOrElse(
+                                            Pointer::incrementCounter,
+                                            () -> pointers.add(new Pointer(path)));
+                            pointers.addAll(val);
+                            return pointers;
+                        }
+                    });
+                }
+            } catch (IOException e) {
+                System.err.println("call(): Error has occurred. message: " + e.getMessage());
+            }
+            return null;
+        }
+    }
+}
+
+class Pointer {
+    private Integer counter;
+    private final String filePath;
+
+    public Pointer(String filePath) {
+        this.counter = 1;
+        this.filePath = filePath;
+    }
+
+    public void incrementCounter() {
+        this.counter++;
+    }
+
+    public Integer getCounter() {
+        return this.counter;
+    }
+
+    public String getFilePath() {
+        return this.filePath;
+    }
+
+    @Override
+    public String toString() {
+        return "{" + "counter=" + counter + ", filePath='" + filePath + '\'' + '}';
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
+import java.util.concurrent.Executors;
 
 public class Main {
     public static void main(String[] args) {
+        Index index = new Index(Executors.newFixedThreadPool(3));
+//        index.indexAllTxtInPath("/path/to/dir/");
     }
 }
